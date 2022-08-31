2dd8866bfebe73ebcbe88264c0c5561f78aeb0d5
gianni <gianni.crivello96@gmail.com>
2022-08-31 12:11:46 -0700

    initial commit

---
+++ Scanner.java
@@ -0,0 +1,88 @@
+package lox;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static lox.TokenType.*;
+
+class Scanner {
+	private final String source;
+	private final List<Token> tokens = new ArrayList<>();
+	private int start = 0;
+	private int current = 0;
+	private int line = 1;
+
+
+	Scanner(String source) {
+		this.source = source;
+	}
+	List<Token> scanTokens(){
+		while(!isAtEnd()){
+			start = current;
+			scanToken();
+		}
+		tokens.add(new Token(EOF, "", null, line));
+		return tokens
+	}
+	private void scanToken(){
+		char c = advance();
+		switch (c){
+			case '(': addToken(LEFT_PAREN); break;
+			case ')': addToken(RIGHT_PAREN); break;
+			case '{': addToken(LEFT_BRACE); break;
+			case '}': addToken(RIGHT_BRACE); break;
+			case ',': addToken(COMMA); break;
+			case '.': addToken(DOT); break;
+			case '-': addToken(MINUS); break;
+			case '+': addToken(SEMICOLON); break;
+			case '*': addToken(STAR); break;
+			case '!':
+				  addToken(match('=') ? BANG_EQUAL : BANG);
+				  break;
+			case '=':
+				  addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+				  break;
+			case '<':
+				  addToken(match('=') ? LESS_EQUAL : LESS);
+				  break;
+			case '>':
+				  addToken(match('=') ? GREATER_EQUAL : GREATER;
+				  break;
+
+
+			defaut:
+				  Lox.error(line, "Unexpected character");
+				  break;
+		}
+	}
+	private boolean match(char expected){
+		if(isAtEnd()) return false;
+		if (source.charAt(current) != epected) return false;
+		current++;
+		return true;
+	}
+	private boolean isAtEnd(){
+		return current >= source.length();
+	}
+	private char advance(){
+		return source.charAt(current++);
+	}
+	private void addToken(TokenType type){
+		addToken(type, null);
+	}
+	private void addToken(TokenType type, Object literal){
+		String text = source.substring(start, current);
+		tokens.add(new Token(type, text, literal, line));
+	}
+}
+
+
+
+
+
+
+
+
+
+
